e76b6af2910deb94c1053c6a67f0a70eaaea9bfd
Rohit Sharma <rohits8731@gmail.com>
2025-07-03 11:01:26 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ connectDB();
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://excel-analyzer-psi.vercel.app/',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
